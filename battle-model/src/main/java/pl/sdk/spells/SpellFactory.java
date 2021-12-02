14d0ca2b0a0b502f8d213910253b5a45e0be8c5f
Przemysław Wieczorek <pwieczorek@psi.pl>
2021-12-02 16:25:38 +0100

    x

---
+++ battle-model/src/main/java/pl/sdk/spells/SpellFactory.java
@@ -0,0 +1,7 @@
+package pl.sdk.spells;
+
+/**
+ * TODO: Describe this class (The first line - until the first dot - will interpret as the brief description).
+ */
+public class SpellFactory
+{}
---
+++ battle-model/src/test/java/pl/sdk/spells/SpellFactoryTest.java
@@ -0,0 +1,6 @@
+package pl.sdk.spells;
+
+class SpellFactoryTest
+{
+
+}
