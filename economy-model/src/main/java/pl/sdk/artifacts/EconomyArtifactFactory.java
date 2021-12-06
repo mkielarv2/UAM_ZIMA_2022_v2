dd2326a6a96088e8d9f2d7ccf87baf37b55b881c
s452645 <bargro6@st.amu.edu.pl>
2021-12-06 18:31:41 +0100

    add more economy artifacts

--- economy-model/src/main/java/pl/sdk/artifacts/EconomyArtifactFactory.java
+++ economy-model/src/main/java/pl/sdk/artifacts/EconomyArtifactFactory.java
@@ -8,15 +8,36 @@ public class EconomyArtifactFactory {
         switch(name) {
             case "Orb of the Firmament":
                 return new EconomyArtifact(ArtifactInfo.ORB_OF_THE_FIRMAMENT, 6000);
-
             case "Orb of Slit":
                 return new EconomyArtifact(ArtifactInfo.ORB_OF_SLIT, 6000);
-
             case "Orb of Tempestuous Fire":
                 return new EconomyArtifact(ArtifactInfo.ORB_OF_TEMPESTUOUS_FIRE, 6000);
-
             case "Orb of Driving Rain":
                 return new EconomyArtifact(ArtifactInfo.ORB_OF_DRIVING_RAIN, 6000);
+            case "Tome of Air Magic":
+                return new EconomyArtifact(ArtifactInfo.TOME_OF_AIR_MAGIC, 20000);
+            case "Tome of Earth Magic":
+                return new EconomyArtifact(ArtifactInfo.TOME_OF_EARTH_MAGIC, 20000);
+            case "Tome of Fire Magic":
+                return new EconomyArtifact(ArtifactInfo.TOME_OF_FIRE_MAGIC, 20000);
+            case "Tome of Water Magic":
+                return new EconomyArtifact(ArtifactInfo.TOME_OF_WATER_MAGIC, 20000);
+            case "Ring of Vitality":
+                return new EconomyArtifact(ArtifactInfo.RING_OF_VITALITY, 5000);
+            case "Vial of Lifeblood":
+                return new EconomyArtifact(ArtifactInfo.VIAL_OF_LIFEBLOOD, 10000);
+            case "Necklace of Swiftness":
+                return new EconomyArtifact(ArtifactInfo.NECKLACE_OF_SWIFTNESS, 5000);
+            case "Cape of Velocity":
+                return new EconomyArtifact(ArtifactInfo.CAPE_OF_VELOCITY, 10000);
+            case "Centaur's Axe":
+                return new EconomyArtifact(ArtifactInfo.CENTAURS_AXE, 2000);
+            case "Titan's Gladius":
+                return new EconomyArtifact(ArtifactInfo.TITANS_GLADIUS, 10000);
+            case "Shield of the Dwarven Lords":
+                return new EconomyArtifact(ArtifactInfo.SHIELD_OF_THE_DWARVEN_LORDS, 2000);
+            case "Sentinel's Shield":
+                return new EconomyArtifact(ArtifactInfo.SENTINELS_SHIELD, 10000);
 
             default:
                 throw new IllegalArgumentException(EXCEPTION_MESSAGE);
--- hero-common/src/main/java/pl/sdk/artifacts/ArtifactInfo.java
+++ hero-common/src/main/java/pl/sdk/artifacts/ArtifactInfo.java
@@ -2,11 +2,27 @@ package pl.sdk.artifacts;
 
 enum ArtifactInfo implements ArtifactInfoIf {
 
-    ORB_OF_THE_FIRMAMENT("Orb of the Firmament", "Hero's air spells to extra 50% damage", ArtifactSlotType.MISC),
-    ORB_OF_SLIT("Orb of Slit", "Hero's earth spells to extra 50% damage", ArtifactSlotType.MISC),
-    ORB_OF_TEMPESTUOUS_FIRE("Orb of Tempestuous Fire", "Hero's fire spells to extra 50% damage", ArtifactSlotType.MISC),
-    ORB_OF_DRIVING_RAIN("Orb of Driving Rain", "Hero's water spells to extra 50% damage", ArtifactSlotType.MISC);
+    // SPELL
+    ORB_OF_THE_FIRMAMENT("Orb of the Firmament", "Hero's air spells deal extra 50% damage", ArtifactSlotType.MISC),
+    ORB_OF_SLIT("Orb of Slit", "Hero's earth spells deal extra 50% damage", ArtifactSlotType.MISC),
+    ORB_OF_TEMPESTUOUS_FIRE("Orb of Tempestuous Fire", "Hero's fire spells deal extra 50% damage", ArtifactSlotType.MISC),
+    ORB_OF_DRIVING_RAIN("Orb of Driving Rain", "Hero's water spells deal extra 50% damage", ArtifactSlotType.MISC),
+    TOME_OF_AIR_MAGIC("Tome of Air Magic", "All air spells are available to hero when equipped", ArtifactSlotType.MISC),
+    TOME_OF_EARTH_MAGIC("Tome of Earth Magic", "All earth spells are available to hero when equipped", ArtifactSlotType.MISC),
+    TOME_OF_FIRE_MAGIC("Tome of Fire Magic", "All fire spells are available to hero when equipped", ArtifactSlotType.MISC),
+    TOME_OF_WATER_MAGIC("Tome of Water Magic", "All water spells are available to hero when equipped", ArtifactSlotType.MISC),
 
+    // COMBAT
+    RING_OF_VITALITY("Ring of Vitality", "Increases health of all your units by 1", ArtifactSlotType.FINGERS),
+    VIAL_OF_LIFEBLOOD("Vial of Lifeblood", "Increases health of all your units by 2", ArtifactSlotType.MISC),
+    NECKLACE_OF_SWIFTNESS("Necklace of Swiftness", "Increases the combat speed of all your units by 1", ArtifactSlotType.NECK),
+    CAPE_OF_VELOCITY("Cape of Velocity", "Increases the combat speed of all your units by 2", ArtifactSlotType.SHOULDERS),
+
+    // PRIMARY
+    CENTAURS_AXE("Centaur's Axe", "+2 attack skill", ArtifactSlotType.RIGHT_HAND),
+    TITANS_GLADIUS("Titan's Gladius", "+12 attack skill, -3 defence skill", ArtifactSlotType.RIGHT_HAND),
+    SHIELD_OF_THE_DWARVEN_LORDS("Shield of the Dwarven Lords", "+2 defence skill", ArtifactSlotType.LEFT_HAND),
+    SENTINELS_SHIELD("Sentinel's Shield", "+12 defence skill, -3 attack skill", ArtifactSlotType.LEFT_HAND);
 
 
     private final String name;
