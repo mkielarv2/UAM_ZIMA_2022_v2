bcb13cb87a2791fa6f975d32bab4a6ab80f9c980
e-lawniczak <60924751+e-lawniczak@users.noreply.github.com>
2021-12-07 19:04:59 +0100

    Fields 1 (#4)
    * First test, empty class
    
    * Special field tests

--- battle-model/src/test/java/pl/sdk/fields/SpecialFieldTest.java
+++ battle-model/src/test/java/pl/sdk/fields/SpecialFieldTest.java
@@ -1,5 +1,7 @@
 package pl.sdk.fields;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.sdk.GameEngine;
 import pl.sdk.Point;
@@ -14,18 +16,104 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpecialFieldTest {
+
+    private GameEngine engine;
+    private Creature creature;
+    @BeforeEach
+    void init(){
+        creature = NecropolisFactory.createDefaultForTests();
+        engine = new GameEngine(new ArrayList<>(List.of(creature)), Collections.emptyList());
+    }
+
+    @Test
+    void fieldCreated(){
+        SpecialField field = SpecialFieldFactory.createImpassableField();
+
+        engine.createSpecialField(new Point(5,5), field);
+
+        assertEquals(field, engine.getField(new Point(5,5)));
+
+    }
+
     @Test
     void shouldCreatureMoveToImpassableField() {
-        Creature creature = NecropolisFactory.createDefaultForTests();
-        Creature creature2 = NecropolisFactory.createDefaultForTests();
-        GameEngine engine = new GameEngine(new ArrayList<>(List.of(creature)), Collections.emptyList());
 
-        GameEngine.createSpecialField(new Point(5,5), SpecialFieldFactory.createImpassableField());
+
+        engine.createSpecialField(new Point(5,5), SpecialFieldFactory.createImpassableField());
 
         boolean canMove = engine.canMove(5, 5);
 
         assertFalse(canMove);
 
     }
+
+    @Test
+    void shouldCreatureAttackDestructibleField() {
+
+        engine.createSpecialField(new Point(5,5), SpecialFieldFactory.createDestructibleField(3));
+
+        SpecialField field = engine.getField(new Point(5,5));
+
+        assertEquals(field.getHp(), 3);
+
+        int atk = engine.getActiveCreature().getAttack();
+
+        engine.getActiveCreature.attack(field.getObstacle());
+
+        assertEquals(field.getHp(), 3 - atk);
+
+    }
+
+    @Test
+    void shouldFlyingCreatureStepOnDamageField() {
+
+        engine.createSpecialField(new Point(5,5), SpecialFieldFactory.createDamageField(5, MoveType.FLYING));
+
+        SpecialField field = engine.getField(new Point(5,5));
+
+        int prevHp = engine.getActiveCreature().getCurrentHp();
+
+        field.triggerEffect(engine.getActiveCreature());
+
+        int afterHp = engine.getActiveCreature().getCurrentHp();
+
+        assertEquals(afterHp, prevHp - 5);
+
+    }
+    @Test
+    void shouldLandCreatureStepOnDamageField() {
+
+        engine.createSpecialField(new Point(5,5), SpecialFieldFactory.createDamageField(5, MoveType.WALKING));
+
+        SpecialField field = engine.getField(new Point(5,5));
+
+        int prevHp = engine.getActiveCreature().getCurrentHp();
+
+        field.triggerEffect(engine.getActiveCreature());
+
+        int afterHp = engine.getActiveCreature().getCurrentHp();
+
+        assertEquals(afterHp, prevHp - 5);
+
+    }
+    @Test
+    void shouldCreatureStepOnBuff() {
+
+        Spell spell = SpellFactory.createSpeedBuff();
+
+        engine.createSpecialField(new Point(5,5), SpecialFieldFactory.createStatField(spell));
+
+        SpecialField field = engine.getField(new Point(5,5));
+
+        int range = engine.getActiveCreature().getMoveRange();
+
+        field.triggerEffect(engine.getActiveCreature());
+
+        int buffedRange = engine.getActiveCreature().getMoveRange();
+
+        assertEquals(buffedRange, range*2);
+
+    }
+
 
 }
