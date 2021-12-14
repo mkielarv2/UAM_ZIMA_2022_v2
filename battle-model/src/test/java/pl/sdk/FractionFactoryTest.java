bdff72cbbc7a5a44a7a5d649d932918f33461d60
Mateusz Walas <matwal12@st.amu.edu.pl>
2021-12-14 11:49:11 +0100

    add new test to FractionFactoryTest

--- battle-model/src/test/java/pl/sdk/FractionFactoryTest.java
+++ battle-model/src/test/java/pl/sdk/FractionFactoryTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FractionFactoryTest {
 
     @Test
-    public void ShouldCreateFractionInstanceCorrectly() {
+    public void ShouldCreateInstanceOfCastleFraction() {
         FractionFactory test = new FractionFactory();
         AbstractCreatureFactory factory = test.getInstance(Fractions.CASTLE);
 
@@ -16,11 +16,45 @@ public class FractionFactoryTest {
     }
 
     @Test
-    public void ShouldNotCreateOtherFractionInstance() {
+    public void ShouldNotCreateOtherInstaceFractionThanCastle() {
         FractionFactory test = new FractionFactory();
         AbstractCreatureFactory factory = test.getInstance(Fractions.CASTLE);
 
         assertFalse(factory instanceof DungeonFactory);
         assertFalse(factory instanceof NecropolisFactory);
+    }
+
+    @Test
+    public void ShouldCreateInstanceOfDungeonFraction() {
+        FractionFactory test = new FractionFactory();
+        AbstractCreatureFactory factory = test.getInstance(Fractions.DUNGEON);
+
+        assertTrue(factory instanceof DungeonFactory);
+    }
+
+    @Test
+    public void ShouldNotCreateOtherInstaceFractionThanDungeon() {
+        FractionFactory test = new FractionFactory();
+        AbstractCreatureFactory factory = test.getInstance(Fractions.DUNGEON);
+
+        assertFalse(factory instanceof CastleFactory);
+        assertFalse(factory instanceof NecropolisFactory);
+    }
+
+    @Test
+    public void ShouldCreateInstanceOfNecropolisFraction() {
+        FractionFactory test = new FractionFactory();
+        AbstractCreatureFactory factory = test.getInstance(Fractions.NECROPOLIS);
+
+        assertTrue(factory instanceof NecropolisFactory);
+    }
+
+    @Test
+    public void ShouldNotCreateOtherInstaceFractionThanNecropolis() {
+        FractionFactory test = new FractionFactory();
+        AbstractCreatureFactory factory = test.getInstance(Fractions.NECROPOLIS);
+
+        assertFalse(factory instanceof DungeonFactory);
+        assertFalse(factory instanceof CastleFactory);
     }
 }
